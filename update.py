b52b8e85ea142ddc2a2550216746ee5f643c41f9
smrutiigit <106029838+smrutiigit@users.noreply.github.com>
2022-06-06 06:15:11 +0530

    Update update.py

--- update.py
+++ update.py
@@ -1,19 +1,32 @@
 import json
-def write_json(new_data, filename='emp.json'):
-	with open(filename,'r+') as file:
-		file_data = json.load(file)
-		file_data["emp_details"].append(new_data)
-		file.seek(0)
-		json.dump(file_data, file, indent = 4)
 
-	# python object to be appended
-y = {"name" : input("Enter employee name : "),
-      "emp_id" : "2022-001",
-      "city" : input ("Enter city : "),
-      "experience" : int(input("Enter experience :")),
-      "ctc" : int(input("Enter CTC : ")),
-      "age" : int(input("Enter age : ")),
-      "contact" : int(input("Enter contact : "))
-    }
+filename = "emp.json"
+def Choices():
+	print ("Manage Employee details")
+	print ("(y) Add New Employee details")
+	print ("(n) Exit")
 	
-write_json(y)
+
+def add_data():
+	item_data = {}
+	with open (filename, "r") as f:
+		temp = json.load(f)
+	item_data["name"] = input("Enter employee name:")
+	item_data["emp ID"] = 2022-0
+	item_data["city"] = input("Enter City: ")
+	item_data["expiriance"] = input("Enter exprience: ")
+	item_data["ctc"] = input("Enter CTC: ")
+	item_data["age"] = input("Enter age: ")
+	item_data["contact"] = input("Enter contact: ")
+	temp.append(item_data)
+	with open(filename, "w") as f:
+          json.dump( temp,f, indent=4)
+          print(item_data)
+while True:
+	Choices()
+	Choice = input ("\nSelect a option: ")
+	if Choice == "y":
+		add_data()
+	if Choice == "n":
+		break
+	
