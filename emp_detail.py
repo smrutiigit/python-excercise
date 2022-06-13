bf76a23297932bdb2ed5147348f5cca6e6cea982
smrutiigit <106029838+smrutiigit@users.noreply.github.com>
2022-06-13 06:17:22 +0530

    Update emp_detail.py

--- emp_detail.py
+++ emp_detail.py
@@ -1,14 +1,14 @@
 import json
 from textwrap import indent
-
-print("Manage Employee Detail: ")
-print("(a) for Add")
-print("(u) for Update")
-print("(d) for Delete")
-Choice = (input("Enter Your choice: "))
+while True:
+ print("Manage Employee Detail: ")
+ print("(a) for Add")
+ print("(u) for Update")
+ print("(d) for Delete")
+ Choice = (input("Enter Your choice: "))
 #condition for ADD
-if Choice=="a" :
-    emp_dic =[{
+ if Choice=="a" :
+     emp_dic =[{
       "name" : input("Enter employee name : "),
       "emp_id" : "2022-001",
       "city" : input ("Enter city : "),
@@ -18,17 +18,17 @@ if Choice=="a" :
       "contact" : int(input("Enter contact : ")) 
       }]
     #read & append the data.
-    with open ("emp.json","r") as file:
+     with open ("emp.json","r") as file:
         data=json.load(file)
         data.append(emp_dic)
     #write the data in json file & display it.
-    with open("emp.json","w") as file:
-     json.dump(data, file , indent=4)
-    print(data)
+     with open("emp.json","w") as file:
+      json.dump(data, file , indent=4)
+     print(data)
 
 #condition for UPDATE
-elif Choice=="u" :
-  #show the existing file
+ elif Choice=="u" :
+  #read and show the existing file
     jsonfile=open("emp.json","r")
     data=json.load(jsonfile)
     print(data)
@@ -38,15 +38,16 @@ elif Choice=="u" :
     jsonfile=open("emp.json","r")
     data=json.load(jsonfile)
 
-   #check the id exists or not 
-    if enter_id== ["emp_id"]:
-        data["name"] = input("Enter employee name : ")
-        data["emp_id"] = "2022-001"
-        data["city"] = input ("Enter city : ")
-        data["experience"]= int(input("Enter experience :"))
-        data["ctc"] = int(input("Enter CTC : "))
-        data["age"] = int(input("Enter age : "))
-        data["contact"] = int(input("Enter contact : ")) 
+   #check the id exists or not
+    for idx, emp in enumerate(data):
+     if enter_id == emp[0]['emp_id']:
+        data["idx"]["name"] = input("Enter employee name : ")
+        data["idx"]["emp_id"] = "2022-001"
+        data["idx"]["city"] = input ("Enter city : ")
+        data["idx"]["experience"]= int(input("Enter experience :"))
+        data["idx"]["ctc"] = int(input("Enter CTC : "))
+        data["idx"]["age"] = int(input("Enter age : "))
+        data["idx"]["contact"] = int(input("Enter contact : ")) 
         jsonfile =open("emp.json", "w") 
         json.dump(data, jsonfile, indent=4)
         print(data)
@@ -54,7 +55,7 @@ elif Choice=="u" :
       print("emp_id is not found.")
       
 #condition for Delete
-elif Choice == "d":
+ elif Choice == "d":
   #show the existing file
     jsonfile=open("emp.json","r")
     data=json.load(jsonfile)
@@ -67,18 +68,34 @@ elif Choice == "d":
     jsonfile.close()
 
     #check the id exists or not
-    if enter_id==[{'emp_id'}]:
+    for idx, emp in enumerate(data):
+     if enter_id == emp[0]["emp_id"]:
     #asking for confirmation
         enter_confirmation =input("Are you want to delete,if yes enter (y): ")
         if enter_confirmation == "y":
-          del data[enter_id]
+          del data[idx]
     else:
       print("emp_id is not found.")
 
 #when the input is other than a,u,d:
-else:
+ else:
     print("invalid input: ")
 
+ choice= input("Do you want to go back to main menu list,enter back: ")
+ if choice!="back":
+   break
+ print(data)
+
+
+
+
+ 
+     
+
+
+
+      
+      
 
 
  
